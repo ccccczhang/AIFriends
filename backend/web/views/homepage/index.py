886dbc7fdf2eccce5b09eaf5ae1d86a3e58965f0
cheng zhang <3426458716@qq.com>
2026-02-24 12:48:04 +0800

    实现搜索功能

--- backend/web/views/homepage/index.py
+++ backend/web/views/homepage/index.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -8,7 +9,14 @@ class HomepageIndexView(APIView):
     def get(self, request):
         try:
             items_count = int(request.query_params.get('items_count', 0)) #  加上 0，意味如果items_count没传参，则默认值为0
-            characters_raw = Character.objects.all().order_by('-id')[items_count: items_count + 20] #从 Character 表中按 id 倒序跳过前 items_count 条再取接下来的 20 条数据
+            search_query = request.query_params.get('search_query', '').strip()  #  后端处理查询操作
+            if search_query:
+                queryset = Character.objects.filter(
+                    Q(name__icontains=search_query) | Q(profile__icontains=search_query) # Q:django中利用 Q进行复杂查询 i:不区分大小写，contains：包含
+                )
+            else:
+                queryset = Character.objects.all()
+            characters_raw = queryset.order_by('-id')[items_count: items_count + 20] #从 Character 表中按 id 倒序跳过前 items_count 条再取接下来的 20 条数据
             characters = []
             for character in characters_raw:
                 author = character.author
--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -6,8 +6,29 @@ import CreateIcon from "@/components/navbar/icons/CreateIcon.vue";
 import SearchIcon from "@/components/navbar/icons/SearchIcon.vue";
 import {useUserStore} from "@/stores/user.js";
 import UserMenu from "@/components/navbar/UserMenu.vue";
+import {ref, watch} from "vue";
+import {useRoute, useRouter} from "vue-router";
 
 const user = useUserStore()
+// 处理查询操作
+const searchQuery = ref('')
+const router = useRouter() // 前进 后退
+const route = useRoute()
+
+// watch(监听源, 回调函数)
+watch(() => route.query.q, newQ => {
+  searchQuery.value = newQ || '' // newQ可能不存在或者为null，保证安全加上 || ''，兜底写法
+})
+
+function handleSearch() {
+  router.push({
+    name: 'homepage-index',
+    query: {
+      q: searchQuery.value.trim(),
+    }
+  })
+}
+
 </script>
 
 <template>
@@ -22,13 +43,14 @@ const user = useUserStore()
           <div class="px-2 font-bold text-xl">AIFriends</div>
         </div>
         <div class="navbar-center w-4/5 max-w-180 flex justify-center">
-          <div class="join w-4/5 flex justify-center">
-            <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
+          <!-- form表单：向 Web 服务器提交信息，把输入内容放到url上 .prevent：防止浏览器刷新页面-->
+          <form @submit.prevent="handleSearch" class="join w-4/5 flex justify-center">
+            <input v-model="searchQuery" class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
             <button class="btn join-item rounded-r-full gap-0">
               <SearchIcon />
               搜索
             </button>
-          </div>
+          </form>
         </div>
         <div class="navbar-end">
           <RouterLink v-if="user.isLogin()" :to="{name: 'create-index'}" active-class="btn-active" class="btn btn-ghost text-base mr-6">
