6d17b1202d343d22b4d99c46ef8cf31e964dac69
cheng zhang <3426458716@qq.com>
2026-02-27 00:19:36 +0800

    实现好友页面后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,12 +1,19 @@
+# 后台管理系统，这里是在：定制后台页面怎么用
 from django.contrib import admin
 from web.models.user import UserProfile
 from web.models.character import Character
+from web.models.friend import Friend
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',) #逗号不可删，表示传的是列表
-
+@admin.register(UserProfile) #注册模型到后台
+class UserProfileAdmin(admin.ModelAdmin): #告诉 Django：admin 后台要管理 UserProfile 这张表
+    raw_id_fields = ('user',)
+#逗号不可删，表示传的是列表，raw_id_fields是后台显示方式设置，如果不写会显示下拉框，显示所有用户，用户多了就会卡爆，写了的话，就会变成一个 ID 输入框 + 搜索按钮
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
---
+++ backend/web/models/friend.py
@@ -0,0 +1,18 @@
+# 每个用户与每个虚拟角色的好友关系
+from django.db import models
+from django.utils.timezone import now, localtime
+
+from web.models.character import Character
+from web.models.user import UserProfile
+
+
+class Friend(models.Model):
+    me = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    # 给大模型的短期记忆，给大模型总结一下与虚拟人物的关键信息
+    memory = models.TextField(default='', max_length=5000, blank=True, null=True)
+    create_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.character.name} - {self.me.user.username} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}" #strftime
