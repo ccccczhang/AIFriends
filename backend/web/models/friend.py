d21138697123e950c2f27b5a7e115126f66d5586
cheng zhang <3426458716@qq.com>
2026-03-04 01:19:13 +0800

    创建消息数据库

--- backend/web/models/friend.py
+++ backend/web/models/friend.py
@@ -17,3 +17,16 @@ class Friend(models.Model):
 
     def __str__(self):
         return f"{self.character.name} - {self.me.user.username} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}" #strftime
+
+class Message(models.Model):
+    friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+    user_message = models.TextField(max_length=500)
+    input = models.TextField(max_length=500) # 对大模型的输入
+    output = models.TextField(max_length=500) # 对大模型的输出
+    input_tokens = models.IntegerField(default=0) # 输入的 tokens Integer: 整数int
+    output_tokens = models.IntegerField(default=0) # 输出的 tokens
+    total_tokens = models.IntegerField(default=0) #总共的 tokens
+    create_time = models.DateTimeField(default=now)
+    # 方便在数据中看
+    def __str__(self):                                                                             #输出前50个
+        return f"{self.friend.character.name} - {self.friend.me.user.username} - {self.user_message[:50]} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
